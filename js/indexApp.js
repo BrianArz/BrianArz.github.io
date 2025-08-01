a9cbfaaa4448f0c3113b776bd3cbe9e1972c5a2b
Brian Arizmendi <arz.dev@outlook.com>
2025-08-01 15:31:35 -0600

    pick and run

--- js/indexApp.js
+++
@@ -1,9 +0,0 @@
-const app = Vue.createApp({
-    methods: {
-        goToCategory(category) {
-            window.location.href = `catalog.html?category=${category}`;
-        },
-    },
-});
-
-app.mount('#indexApp');
